9e95a58c5823c993d474648d2b60f597af050dc4
NicholasSlaughter <38059177+NicholasSlaughter@users.noreply.github.com>
2020-11-11 15:37:23 -0800

    Established Database Connection

--- ExpenseTracker/ExpenseTracker/ExpenseTracker.py
+++ ExpenseTracker/ExpenseTracker/ExpenseTracker.py
@@ -1,1 +1,13 @@
+import pyodbc
+from DatabaseConnection import DbConnection
 
+cursor = DbConnection.conxn.cursor()
+
+#grabs data to output using a query
+cursor.execute('SELECT * FROM Category')
+
+#print data grabbed from query
+for row in cursor:
+    print(row)
+
+DbConnection.conxn.close()
