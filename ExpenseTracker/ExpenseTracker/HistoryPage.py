1e518fda30680ddb4020aec533cdb013e3eb2954
NicholasSlaughter <38059177+NicholasSlaughter@users.noreply.github.com>
2020-11-12 15:31:33 -0800

    Add Functionality of Deleting from the Expense and Alert tables in the database.

--- ExpenseTracker/ExpenseTracker/HistoryPage.py
+++ ExpenseTracker/ExpenseTracker/HistoryPage.py
@@ -13,36 +13,61 @@ class HistoryPageForm(Toplevel):
         history_tree = ttk.Treeview(self)
 
         #Establish Column Names
-        history_tree['columns'] = ('Category','Price','Date')
+        history_tree['columns'] = ('Id','Category','Price','Date')
 
         #Format Columns
-        history_tree.column('#0',width=0) #Phantom column (if parent and child then have width greater than 0)
+        history_tree.column('#0',width=0,minwidth=0) #Phantom column (if parent and child then have width greater than 0)
+        history_tree.column('Id',width=0,minwidth=0)
         history_tree.column('Category', anchor=W, width=120)
         history_tree.column('Price', anchor=CENTER, width=80)
         history_tree.column('Date', anchor=W, width=120)
 
         #Create Headings
-        history_tree.heading('#0',text='',anchor=W)
+        history_tree.heading('#0')
+        history_tree.heading('Id')
         history_tree.heading('Category',text='Category', anchor=W)
         history_tree.heading('Price',text='Price', anchor=CENTER)
         history_tree.heading('Date',text='Date', anchor=W)
 
         ### Add Data ###
         #Query to get data from the expense and category tables
-        expenses = cursor.execute('SELECT Category.Name, Expense.Amount, Expense.Date FROM Expense INNER JOIN Category ON Expense.CategoryId=Category.Id').fetchall()
+        expenses = cursor.execute('SELECT Expense.Id, Category.Name, Expense.Amount, Expense.Date FROM Expense INNER JOIN Category ON Expense.CategoryId=Category.Id').fetchall()
 
         #Loop through the expenses and append them in a list
         var = []
         for e in expenses:
-            var.append((e[0],e[1],e[2]))
+            var.append((e[0],e[1],e[2],e[3]))
 
         #Put Data Into Tree
         for i in range(len(var)):
-            history_tree.insert(parent='',index='end',iid=i,values=(var[i][0],var[i][1],var[i][2]))
+            history_tree.insert(parent='',index='end',iid=i,values=(var[i][0],var[i][1],var[i][2],var[i][3]))
 
         #Grid To Screen
         history_tree.grid(row=0,column=0,padx=5,pady=20,sticky='NSEW')
 
+        #Delete Selected Expenses
+        def Remove_Expenses():
+            nonlocal history_tree
+            expenses_to_remove = history_tree.selection()
+            #Loop through the selected expenses and delete them from both the tree and the database
+            for e in expenses_to_remove:
+                #Get the the Id of the expense in the database
+                expense_id = history_tree.item(e)['values']
+                
+                #Set up query used to delete an element from the expense table
+                delete_query = '''DELETE FROM Expense WHERE Id = (?)'''
+
+                #The Id is the 1st element in the array to scope to that and delete it from the db
+                cursor.execute(delete_query,expense_id[0])
+                DbConnection.conxn.commit()
+
+                #delete expense from tree to sync with database
+                history_tree.delete(e)
+
+        #button used to delete an element from the expense table
+        delete_selected_button = Button(self,text="Delete Selected Expenses",command=lambda:Remove_Expenses(),font=20,width=7).grid(row=1,column=0,padx=10,pady=5,sticky='NSEW')
+
         #Add functionality for the widgets to resize with the window
         self.grid_columnconfigure(0,weight=1)
         self.grid_rowconfigure(0,weight=1)
+        self.grid_rowconfigure(1,weight=1)
--- ExpenseTracker/ExpenseTracker/ViewAlertsPage.py
+++ ExpenseTracker/ExpenseTracker/ViewAlertsPage.py
@@ -12,45 +12,70 @@ class ViewAlertsPageForm(Toplevel):
         cursor = DbConnection.conxn.cursor()
 
         #Define Tree View
-        history_tree = ttk.Treeview(self)
+        alert_tree = ttk.Treeview(self)
 
         #Establish Column Names
-        history_tree['columns'] = ('Category','Currently Spent','Max Alloted','Period')
+        alert_tree['columns'] = ('Id','Category','Currently Spent','Max Alloted','Period')
 
         #Format Columns
-        history_tree.column('#0',width=0) #Phantom column (if parent and child then have width greater than 0)
-        history_tree.column('Category', anchor=W, width=120)
-        history_tree.column('Currently Spent', anchor=CENTER, width=120)
-        history_tree.column('Max Alloted', anchor=CENTER, width=120)
-        history_tree.column('Period',anchor=W,width=120)
+        alert_tree.column('#0',width=0,minwidth=0) #Phantom column (if parent and child then have width greater than 0)
+        alert_tree.column('Id',width=0,minwidth=0)
+        alert_tree.column('Category', anchor=W, width=120)
+        alert_tree.column('Currently Spent', anchor=CENTER, width=120)
+        alert_tree.column('Max Alloted', anchor=CENTER, width=120)
+        alert_tree.column('Period',anchor=W,width=120)
 
         #Create Headings
-        history_tree.heading('#0',text='',anchor=W)
-        history_tree.heading('Category',text='Category', anchor=W)
-        history_tree.heading('Currently Spent',text='Currently Spent', anchor=CENTER)
-        history_tree.heading('Max Alloted',text='Max Alloted', anchor=CENTER)
-        history_tree.heading('Period',text='Period',anchor=W)
+        alert_tree.heading('#0')
+        alert_tree.heading('Id')
+        alert_tree.heading('Category',text='Category', anchor=W)
+        alert_tree.heading('Currently Spent',text='Currently Spent', anchor=CENTER)
+        alert_tree.heading('Max Alloted',text='Max Alloted', anchor=CENTER)
+        alert_tree.heading('Period',text='Period',anchor=W)
 
         ### Add Data ###
-        #Query to get data from the expense and category tables
-        alerts = cursor.execute('SELECT Category.Name, Alert.CurrentAmount, Alert.MaxAmount, Period.Name FROM Alert INNER JOIN Category ON Alert.CategoryId=Category.Id INNER JOIN Period ON Alert.PeriodId=Period.Id').fetchall()
+        #Query to get data from the alert and category tables
+        alerts = cursor.execute('SELECT Alert.Id, Category.Name, Alert.CurrentAmount, Alert.MaxAmount, Period.Name FROM Alert INNER JOIN Category ON Alert.CategoryId=Category.Id INNER JOIN Period ON Alert.PeriodId=Period.Id').fetchall()
 
-        #Loop through the expenses and append them in a list
+        #Loop through the alerts and append them in a list
         var = []
         for a in alerts:
-            var.append((a[0],a[1],a[2],a[3]))
+            var.append((a[0],a[1],a[2],a[3],a[4]))
 
         #Put Data Into Tree
         for i in range(len(var)):
-            history_tree.insert(parent='',index='end',iid=i,values=(var[i][0],var[i][1],var[i][2],var[i][3]))
+            alert_tree.insert(parent='',index='end',iid=i,values=(var[i][0],var[i][1],var[i][2],var[i][3],var[i][4]))
 
         #Pack To Screen
-        history_tree.grid(row=0,column=0,padx=5,pady=20,sticky='NSEW')
+        alert_tree.grid(row=0,column=0,padx=5,pady=20,sticky='NSEW')
+
+        #Delete Selected Alerts
+        def Remove_Alerts():
+            nonlocal alert_tree
+            alerts_to_remove = alert_tree.selection()
+            #Loop through the selected alerts and delete them from both the tree and the database
+            for a in alerts_to_remove:
+                #Get the the Id of the alert in the database
+                alert_id = alert_tree.item(a)['values']
+                
+                #Set up query used to delete an element from the alert table
+                delete_query = '''DELETE FROM Alert WHERE Id = (?)'''
+
+                #The Id is the 1st element in the array to scope to that and delete it from the db
+                cursor.execute(delete_query,alert_id[0])
+                DbConnection.conxn.commit()
+
+                #delete alert from tree to sync with database
+                alert_tree.delete(a)
+
+        #button used to delete an element from the alert table
+        delete_selected_button = Button(self,text="Delete Selected Alerts",command=lambda:Remove_Alerts(),font=20,width=7).grid(row=1,column=0,padx=10,pady=5,sticky='NSEW')
 
         #Add Set Alert button that takes you to the Set Alert page
-        set_alert_button = Button(self,text="Set An Alert", command= lambda:SetAlertPageForm(self),font=20,width=7).grid(row=1,column=0,padx=5,pady=5,sticky='NSEW')
+        set_alert_button = Button(self,text="Set An Alert", command= lambda:SetAlertPageForm(self),font=20,width=7).grid(row=1,column=1,padx=5,pady=5,sticky='NSEW')
         
         #Add functionality for the widgets to resize with the window
         self.grid_columnconfigure(0,weight=1)
+        self.grid_columnconfigure(1,weight=1)
         self.grid_rowconfigure(0,weight=1)
         self.grid_rowconfigure(1,weight=1)
