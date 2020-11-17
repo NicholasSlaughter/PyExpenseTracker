61f9c0495be83c48204ea7cce43dfee644d80429
NicholasSlaughter <38059177+NicholasSlaughter@users.noreply.github.com>
2020-11-17 12:43:26 -0800

    Added Message To Pop Up When A User Over Spends.

--- ExpenseTracker/ExpenseTracker/HomePage.py
+++ ExpenseTracker/ExpenseTracker/HomePage.py
@@ -12,6 +12,9 @@ def main():
     
     categories = cursor.execute('SELECT * FROM Category').fetchall()
     var = []
+
+
+
     for c in categories:
         var.append(c[1])
     category = StringVar(root)
@@ -32,11 +35,29 @@ def main():
                 break
 
         insert_query = '''INSERT INTO Expense (Amount,Date,CategoryId)
-                      VALUES (?,?,?)'''
+                          VALUES (?,?,?)'''
 
         cursor.execute(insert_query,amount_to_insert,date,cat_to_insert)
         DbConnection.conxn.commit()
         messagebox.showinfo("Expense Entered","You Have Entered An Expense")
+
+        #Loop through each alert, see if there are matching categories with what was previously entered and then update the current amount that the user has spent
+        alerts = cursor.execute('SELECT * FROM Alert').fetchall()
+        for a in alerts:
+            #If there is an alert with a matching category then increase the current amount for that alert by the amount entered
+            if cat_to_insert == a[3]:
+                a[2] += float(amount_to_insert)
+                #Update the new current amount into the database
+                update_query = '''UPDATE Alert
+                                  SET CurrentAmount = (?)
+                                  WHERE Alert.Id = (?)'''
+                cursor.execute(update_query,a[2],a[0])
+                DbConnection.conxn.commit()
+
+                #If the current amount is larger than the max amount for an alert then alert the user that they have over spent
+                if a[1] < a[2]:
+                    alert_period = cursor.execute('SELECT Period.Name FROM Alert INNER JOIN Period ON Alert.PeriodId = Period.Id').fetchall()
+                    messagebox.showinfo(title="Over Spending!",message="You are currently over your " + alert_period[0][0] + " limit for " + cat + "\nAmount Over Spent: " + str((a[2]-a[1])))
 
 
     amount_label = Label(root,text="Amount:",font=20,padx=10,pady=5).grid(row=0,column=0,sticky='NSEW')
